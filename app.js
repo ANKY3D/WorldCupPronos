c9857eaaa2454114a2b9252a1262cefb71fe97c1
ANKY3D <morowinfr@gmail.com>
2026-06-12 15:54:27 +0200

    Remove dev tool resetAppForTesting

--- app.js
+++ app.js
@@ -519,18 +519,6 @@ async function validateSingleMatch(matchId) {
         btn.disabled = false;
         btn.innerHTML = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="M20 6 9 17l-5-5"/></svg>';
     }
-}
 
-// ============================================
-// DEV TOOLS
-// ============================================
-function resetAppForTesting() {
-    if(confirm("Veux-tu vraiment réinitialiser l'application pour faire un nouveau test ?\n(Ceci effacera juste tes données locales pour te permettre de recommencer)")) {
-        localStorage.removeItem('wcpronos_submitted');
-        localStorage.removeItem('wcpronos_name');
-        localStorage.removeItem('wcpronos_progress');
-        window.location.reload();
-    }
-}
 
 
